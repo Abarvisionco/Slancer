84a44d5ca33a5c08454487335578acfeac3746ed
prodbygodfather <mahdighasemiabari84@gmail.com>
2024-04-18 23:29:15 +0330

    `v0.3.2` - add state and all fields to home page

--- main/views.py
+++ main/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from resume.models import Field
+from resume.models import Field, State
 
 
 # default home page
 def home(request):
     field = Field.objects.filter(best=True)
+    all_field = Field.objects.all()
+    state = State.objects.all()
     context = {
-        'field':field
+        'field':field,
+        'all_field':all_field,
+        'stete':state
     }
     return render(request,'main/home.html', context)
