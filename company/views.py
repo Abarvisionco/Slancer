1200b04e50e1cdddd76435409f9d510d8a7059d8
prodbygodfather <mahdighasemiabari84@gmail.com>
2024-05-11 01:16:42 +0330

    `v1.1.3` - write company add and update view with bug

--- company/views.py
+++ company/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from company.forms import CompanyForm
 from company.models import Company
 from django.contrib.auth.decorators import login_required
-
+from django.shortcuts import HttpResponseRedirect
+from django.urls import  reverse_lazy
 
 @login_required
 def company(request):
@@ -18,8 +19,27 @@ def company(request):
     if request.method == 'POST':
         co, created = Company.objects.get_or_create(user=request.user)
         form = CompanyForm(request.POST, request.FILES, instance=co)
-        # if form.is_valid():
-        #     form.
+        if form.is_valid():
+            co.user = request.user
+            co.company_name = form.cleaned_data['company_name']
+            co.image = form.cleaned_data['image']
+            co.company_url = form.cleaned_data['company_url']
+            co.field = form.cleaned_data['field']
+            co.about = form.cleaned_data['about']
+            co.state = form.cleaned_data['state']
+            co.district = form.cleaned_data['district']
+            co.description = form.cleaned_data['description']
+            co.show_mobile = form.cleaned_data['show_mobile']
+            form.save()
+            return HttpResponseRedirect(reverse_lazy('company'))
+    else:
+        try:
+            co = Company.objects.get(user=request.user)
+            form = CompanyForm(instance=co)
+            id = request.user.id
+        except:
+            form = CompanyForm()
+            id = 0
 
     context = {
         'form': form,
