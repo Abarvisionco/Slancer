4f8352d3c43017a3ae0d800b5c062fe6a03e7439
prodbygodfather <mahdighasemiabari84@gmail.com>
2024-05-13 21:21:25 +0330

    `v2.1.0` - fix footer page

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 # Register your models here.
 
 admin.site.index_title = "پنل مدیریت اسکول لنسر"
-admin.site.site_header = "اَبَر پردازش کمپانی"
+admin.site.site_header = "کمپانی اَبَر پردازش "
