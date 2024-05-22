23b911e9eb7da19a27795b4e5092b796f57dc088
godfather <mahdighasemiabari84@gmail.com>
2024-05-22 15:16:10 +0330

    add delete chat action

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ from chat import views
 
 urlpatterns = [
     path('', views.chat, name="chat"),
-    path('send_resume/<int:chat_room_id>/', views.send_message, name='send_message')
+    path('send_resume/<int:chat_room_id>/', views.send_message, name='send_message'),
+    path('delte_chat/<int:chat_room_id>/', views.delete_chat, name='delete_chat')
 ]
